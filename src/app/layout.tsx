9b25197ec1fa3f0c4eb66b674ba09282f096b0de
Shivendra Hada <shivendrahada24@gmail.com>
2024-03-12 11:14:40 +0000

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Shivendra Hada",
+  description: "An experienced senior software developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Intro from "./components/Intro";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center min-h-screen container mx-auto max-w-3xl">
+      <Intro />
+    </main>
+  );
+}
