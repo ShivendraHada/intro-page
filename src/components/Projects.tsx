4a4e799fcb08c496ea4ccc0f193ed293d8906345
Shivendra Hada <shivendrahada24@gmail.com>
2024-03-13 14:49:00 +0000

    Added Coming soon horizontal rule

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -52,6 +52,14 @@ function Projects() {
           Elastic beanstalk, and AWS Lambda
         </h5>
       </div>
+      <div className="my-5 p-2">
+        <div className="inline-flex items-center justify-center w-full">
+          <hr className="w-64 h-[2px] my-8 bg-purple-200 border-0 rounded-full dark:bg-purple-700" />
+          <span className="absolute px-3 font-medium text-purple-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-purple-900">
+            More! Coming Soon...
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
